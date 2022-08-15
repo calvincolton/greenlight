2e932297633b06cabb8fbcd28065b0cdac6302f2
Calvin Colton <calvincolton@Calvins-MBP-2.hsd1.ca.comcast.net>
2022-08-14 17:13:09 -0700

    fix typo

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -82,7 +82,7 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 		}
 	}
 
-	err = dec.Decode(&stuct{}{})
+	err = dec.Decode(&struct{}{})
 	if err != io.EOF {
 		return errors.New("body must only contain a single JSON value")
 	}
