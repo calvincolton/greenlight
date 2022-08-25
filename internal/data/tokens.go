70f17806851b0e2f4a06ced797c35674ee96eb1b
Calvin Colton <calvincolton@Calvins-MBP-2.hsd1.ca.comcast.net>
2022-08-25 09:26:08 -0700

    feat: user activation - add tokens table, generateToken func and validate token func

---
+++ internal/data/tokens.go
@@ -0,0 +1,51 @@
+package data
+
+import (
+	"crypto/sha256"
+	"encoding/base32"
+	"math/rand"
+	"time"
+
+	"github.com/calvincolton/greenlight/internal/validator"
+)
+
+const (
+	ScopeActivation = "activation"
+)
+
+type Token struct {
+	Plaintext string
+	Hash      []byte
+	UserID    int64
+	Expiry    time.Time
+	Scope     string
+}
+
+func ValidateTokenPlaintext(v *validator.Validator, tokenPlaintext string) {
+	v.Check(tokenPlaintext != "", "token", "must be provided")
+	v.Check(len(tokenPlaintext) == 26, "token", "must be 26 bytes long")
+}
+
+func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token := &Token{
+		UserID: userID,
+		Expiry: time.Now().Add(ttl),
+		Scope:  scope,
+	}
+
+	randomBytes := make([]byte, 16)
+
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
+
+	// sha256.Sum256() function returns an *array* of length 32, so to make it easier to
+	// work with we convert it to a slice using the [:] operator before storing it.
+	hash := sha256.Sum256([]byte(token.Plaintext))
+	token.Hash = hash[:]
+
+	return token, nil
+}
