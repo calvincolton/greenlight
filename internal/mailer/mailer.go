276497f21a355240d96ff78b9232a3cc42e9e8c6
Calvin Colton <calvincolton@Calvins-MBP-2.hsd1.ca.comcast.net>
2022-08-24 17:32:19 -0700

    feat: mail service - background go routine for mailer and background helper

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -58,10 +58,17 @@ func (m Mailer) Send(recipient, templateFile string, data any) error {
 	msg.SetBody("text/plain", plainBody.String())
 	msg.AddAlternative("text/html", htmlBody.String())
 
-	err = m.dialer.DialAndSend(msg)
-	if err != nil {
-		return err
+	// Try sending the email up to three times before aborting and returning the final error
+	for i := 1; i <= 3; i++ {
+		err = m.dialer.DialAndSend(msg)
+		// If everything worked, return nil.
+		if nil == err {
+			return nil
+		}
+
+		// If it didn't work, sleep for a short time and retry.
+		time.Sleep(500 * time.Millisecond)
 	}
 
-	return nil
+	return err
 }
