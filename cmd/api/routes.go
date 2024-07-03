f1179b250fc93226853890fe5f3a17cb4737b7b5
Calvin Colton <calvincolton@Calvins-MBP-2.hsd1.ca.comcast.net>
2024-07-02 19:42:49 -0700

    remove vendoring

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -14,8 +14,20 @@ func (app *application) routes() http.Handler {
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
+	// OpenAPI/swagger
+	// Serve the Swagger UI files
+	router.ServeFiles("/swagger-ui/*filepath", http.Dir("swagger-ui/dist"))
+	router.GET("/swagger.yaml", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		http.ServeFile(w, r, "swagger.yaml")
+	})
+	router.GET("/docs", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		http.Redirect(w, r, "/swagger-ui/index.html?url=/swagger.yaml", http.StatusFound)
+	})
+
+	// healthcheck
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
+	// movies
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.requirePermission(store.PermissionMoviesWrite, app.createMovieHandler))
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:movieID", app.showMovieHandler)
@@ -26,9 +38,11 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:movieID", app.requirePermission(store.PermissionMoviesWrite, app.patchMovieHandler))
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:movieID", app.requirePermission(store.PermissionMoviesWrite, app.deleteMovieHandler))
 
+	// users
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
+	// authentication
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticaitonTokenHandler)
 
 	// TODO: hide route via load balancer / reverse proxy so only available locally
