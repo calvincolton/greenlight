eb152489d2951d1cfdabf9bf187ab94e5aff77d9
Calvin Colton <calvincolton@Calvins-MBP-2.hsd1.ca.comcast.net>
2024-07-08 13:24:13 -0700

    prep -- notFoundHandler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"expvar"
 	"net/http"
 
@@ -8,14 +9,16 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+//go:embed swagger.yaml
+var swaggerYAML []byte
+
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
-	router.NotFound = http.HandlerFunc(app.notFoundResponse)
-	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
-
+	// healthcheck
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
+	// movies
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.requirePermission(store.PermissionMoviesWrite, app.createMovieHandler))
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:movieID", app.showMovieHandler)
@@ -26,13 +29,19 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:movieID", app.requirePermission(store.PermissionMoviesWrite, app.patchMovieHandler))
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:movieID", app.requirePermission(store.PermissionMoviesWrite, app.deleteMovieHandler))
 
+	// users
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
+	// authentication
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticaitonTokenHandler)
 
+	// metrics
 	// TODO: hide route via load balancer / reverse proxy so only available locally
 	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
+
+	router.NotFound = http.HandlerFunc(app.notFoundHandler)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
 }
